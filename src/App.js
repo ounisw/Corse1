d87939bb6c0f5338ff4aebff500e6edb40a5b87c
walid ounis <ounis.walid@gmail.com>
2021-10-09 20:46:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Wheather from "./wheather";
+const wheatherToday = fetch(
+  "api.openweathermap.org/data/2.5/weather?q=Algiers,dz&appid=383d93fae88042bc86d6fe1befdeb0cf"
+);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wheather wheatherToday={wheatherToday} />
     </div>
   );
 }
